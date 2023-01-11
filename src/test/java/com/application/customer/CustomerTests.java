391e19ae9683cf017b7329a573b189fd9fe78bcf
naiomi <Naiomi.Naidoo@icloud.com>
2023-01-11 11:56:47 +0100

    Updated tests

--- src/test/java/com/application/customer/CustomerTests.java
+++ src/test/java/com/application/customer/CustomerTests.java
@@ -11,14 +11,6 @@ public class CustomerTests {
 
     @Autowired
     private CustomerRepository customerRepository;
-
-    @Test
-    public void saveNewCustomer() {
-        System.out.println(customerRepository.save(new Customer(1, "NAIOMI")));
-        System.out.println(customerRepository.save(new Customer(2, "EWALD")));
-        System.out.println(customerRepository.save(new Customer(3, "TEYTE")));
-    }
-
     @Test
     public List<Customer> getCustomers() {
         return customerRepository.findAll();
