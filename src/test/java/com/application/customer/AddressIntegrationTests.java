33ee07f39206d77feae98ef4d373924d9720b008
naiomi <Naiomi.Naidoo@icloud.com>
2023-10-23 18:41:54 +0200

    Refactor Address endpoint and update test

--- src/test/java/com/application/customer/AddressIntegrationTests.java
+++ src/test/java/com/application/customer/AddressIntegrationTests.java
@@ -1,2 +1,36 @@
-package com.application.customer;public class AddressIntegrationTests {
+package com.application.customer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+public class AddressIntegrationTests {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @BeforeEach
+    void setup() {
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .build();
+    }
+
+    @Test
+    public void getAddressById_StatusOK() throws Exception{
+        mockMvc.perform(get("/address/2").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+    }
 }
