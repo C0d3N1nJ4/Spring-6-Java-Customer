bccb6fce6719777e1cb1deca79cb5ee95ac66f66
naiomi <Naiomi.Naidoo@icloud.com>
2023-11-25 10:34:57 +0100

    Using Lombok @Data to reduce boilerplate code

--- src/main/java/com/application/contact/Contact.java
+++ src/main/java/com/application/contact/Contact.java
@@ -2,7 +2,9 @@ package com.application.contact;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Data;
 
+@Data
 @Entity
 public class Contact {
 
@@ -12,37 +14,4 @@ public class Contact {
     private String email;
 
     private String phone;
-
-    public Contact(String id, String email, String phone) {
-        this.id = id;
-        this.email = email;
-        this.phone = phone;
-    }
-
-    public Contact() {
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
 }
