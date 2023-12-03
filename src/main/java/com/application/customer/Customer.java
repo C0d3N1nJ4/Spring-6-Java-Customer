47e3cee43c5fd860fd45edf958ed53c9e66b2c8b
naiomi <Naiomi.Naidoo@icloud.com>
2023-12-03 10:21:26 +0100

    Added Lombok annotations to Customer entity

--- src/main/java/com/application/customer/Customer.java
+++ src/main/java/com/application/customer/Customer.java
@@ -3,9 +3,12 @@ package com.application.customer;
 import com.application.address.Address;
 import com.application.contact.Contact;
 import jakarta.persistence.*;
+import lombok.Data;
 
+@Data
 @Entity
 public class Customer {
+
     @Id
     private int id;
     private String name;
@@ -18,63 +21,4 @@ public class Customer {
     @OneToOne
     private Contact contact;
 
-    public Customer() {
-    }
-
-    public Customer(int id, String name, String lastname, String status, Address address, Contact contact) {
-        this.id = id;
-        this.name = name;
-        this.lastname = lastname;
-        this.status = status;
-        this.address = address;
-        this.contact = contact;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    public Contact getContact() {
-        return contact;
-    }
-
-    public void setContact(Contact contact) {
-        this.contact = contact;
-    }
-}
+   }
