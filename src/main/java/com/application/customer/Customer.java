3dbfd0e3d20937b80443a30474a5f995cf9e8dd9
Naiomi Naidoo <naiomi.naidoo@icloud.com>
2023-12-20 02:16:44 +0100

    Refactored Customer for Lombok annotation

--- src/main/java/com/application/customer/Customer.java
+++ src/main/java/com/application/customer/Customer.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
+@Data
 @Table(name = "customer")
 public class Customer {
 
@@ -21,81 +22,5 @@ public class Customer {
 
     @OneToOne
     private Contact contact;
-
-    public Customer() {
-    }
-
-    public Customer(String id, String name, String lastname, String status, Address address, Contact contact) {
-        this.id = id;
-        this.name = name;
-        this.lastname = lastname;
-        this.status = status;
-        this.address = address;
-        this.contact = contact;
-    }
-
-    public Customer(String id, String name, String lastname, String status, Address address) {
-        this.id = id;
-        this.name = name;
-        this.lastname = lastname;
-        this.status = status;
-        this.address = address;
-    }
-
-    public Customer(String id, String name, String lastname, String status) {
-        this.id = id;
-        this.name = name;
-        this.lastname = lastname;
-        this.status = status;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public Contact getContact() {
-        return contact;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name= name;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    public void setContact(Contact contact) {
-        this.contact = contact;
-    }
-
 
 }
